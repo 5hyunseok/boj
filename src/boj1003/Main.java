143c482518948f43b2d0b43f2dd55dbc900c80ca
OHyunseok <5xhyunseok@gmail.com>
2018-07-25 12:07:55 +0900

    Solve 1003

---
+++ src/boj1003/Main.java
@@ -0,0 +1,39 @@
+package boj1003;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+        int n = Integer.valueOf(br.readLine());
+        for(int i=0; i<n; i++){
+           String[] input = br.readLine().split(" ");
+           int result;
+           int x1 = Integer.valueOf(input[0]);
+           int y1 = Integer.valueOf(input[1]);
+           int r1 = Integer.valueOf(input[2]);
+           int x2 = Integer.valueOf(input[3]);
+           int y2 = Integer.valueOf(input[4]);
+           int r2 = Integer.valueOf(input[5]);
+
+           double d = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+           int diff = Math.abs(r1 - r2);
+
+           if(d==0) {
+               if(r1 == r2) result = -1;
+               else result = 0;
+           } else {
+               if(d > (r1+r2) || d < diff) result = 0;
+               else if(d == (r1+r2) || d == diff) result = 1;
+               else result = 2;
+           }
+
+           System.out.println(result);
+        }
+    }
+}
+
+
