56e7192e37e636f7d27e855d099aae8d405c1944
OHyunseok <5xhyunseok@gmail.com>
2018-07-25 13:12:39 +0900

    template

---
+++ src/template/Main.java
@@ -0,0 +1,13 @@
+package template;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+
+    }
+}
