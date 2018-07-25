e349d962af95c15c2d0b3f99fdb866bca591586d
OHyunseok <5xhyunseok@gmail.com>
2018-07-25 11:29:32 +0900

    Solve 1000

---
+++ src/boj1000/Main.java
@@ -0,0 +1,16 @@
+package boj1000;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//    BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+        String[] input = br.readLine().split(" ");
+        System.out.println(Integer.valueOf(input[0]) + Integer.valueOf(input[1]));
+    }
+}
+
+
