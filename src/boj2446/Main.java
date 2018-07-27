bc40d9ac8344b3c537d478b525acc9b61683d14c
OHyunseok <5xhyunseok@gmail.com>
2018-07-27 17:52:33 +0900

    Solve 2446

---
+++ src/boj2446/Main.java
@@ -0,0 +1,24 @@
+package boj2446;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+
+        int n = Integer.valueOf(br.readLine());
+        for(int i=n; i>1; i--) {
+            for(int j=0; j<n-i; j++) System.out.print(" ");
+            for(int j=0; j<(2*i-1); j++) System.out.print("*");
+            System.out.println();
+        }
+        for(int i=1; i<=n; i++) {
+            for(int j=0; j<n-i; j++) System.out.print(" ");
+            for(int j=0; j<(2*i-1); j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
