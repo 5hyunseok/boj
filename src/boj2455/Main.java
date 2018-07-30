314ca5aa022a83fbf224c0faafd4d675435afb4c
OHyunseok <5xhyunseok@gmail.com>
2018-07-30 10:31:56 +0900

    Solve 2455

--- src/boj2455/Main.java
+++ src/boj2455/Main.java
@@ -1,4 +1,22 @@
 package boj2455;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+
+        int max = 0;
+        int count = 0;
+        for(int i=0; i<4; i++){
+            String[] input = br.readLine().split(" ");
+            count -= Integer.valueOf(input[0]);
+            count += Integer.valueOf(input[1]);
+            if(count > max) max = count;
+        }
+        System.out.println(max);
+    }
 }
