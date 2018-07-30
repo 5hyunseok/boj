7e256e92eb65f45dd884e7a27df1c84cde067207
OHyunseok <5xhyunseok@gmail.com>
2018-07-30 14:00:10 +0900

    Solve 2742

---
+++ src/boj2742/Main.java
@@ -0,0 +1,14 @@
+package boj2742;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+
+        int n = Integer.valueOf(br.readLine());
+        for(int i=n; i>0; i--) System.out.println(i);
+    }
+}
