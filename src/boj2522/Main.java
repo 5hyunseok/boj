782d01286a224f7c794c435e9dce96f9977997b5
OHyunseok <5xhyunseok@gmail.com>
2018-07-30 12:00:15 +0900

    Solve 2522

---
+++ src/boj2522/Main.java
@@ -0,0 +1,24 @@
+package boj2522;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+
+        int n = Integer.valueOf(br.readLine());
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<n-i; j++) System.out.print(" ");
+            for(int j=0; j<i; j++) System.out.print("*");
+            System.out.println();
+        }
+        for(int i=n; i>0; i--) {
+            for(int j=0; j<n-i; j++) System.out.print(" ");
+            for(int j=0; j<i; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
