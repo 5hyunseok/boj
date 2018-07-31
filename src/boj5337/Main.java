a1b43aacd0bee6b251f16cffac0c66dd7f5588b4
OHyunseok <5xhyunseok@gmail.com>
2018-07-31 11:06:18 +0900

    Solve 5337

---
+++ src/boj5337/Main.java
@@ -0,0 +1,11 @@
+package boj5337;
+
+public class Main {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n");
+        sb.append("|  | _ | _. _ ._ _  _\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb.toString());
+    }
+}
