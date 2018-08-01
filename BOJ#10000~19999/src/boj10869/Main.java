9e1f5a504eed388d5b4f4086bf9c76f1881fc381
OHyunseok <5xhyunseok@gmail.com>
2018-08-01 17:15:33 +0900

    Solve 10869

---
+++ BOJ#10000~19999/src/boj10869/Main.java
@@ -0,0 +1,22 @@
+package boj10869;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+
+        String[] input = br.readLine().split(" ");
+        int a = Integer.valueOf(input[0]);
+        int b = Integer.valueOf(input[1]);
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
