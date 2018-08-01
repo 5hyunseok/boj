6bb6b7b28b27806851b736bd4724f7ce43d0f34c
OHyunseok <5xhyunseok@gmail.com>
2018-08-01 17:32:58 +0900

    Solve 10171

---
+++ BOJ#10000~19999/src/boj10171/Main.java
@@ -0,0 +1,12 @@
+package boj10171;
+
+public class Main {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+        System.out.println(sb.toString());
+    }
+}
