0c237e1df571a636fc007d4c8d9d0218b65d0f7f
5hyunseok <5xhyunseok@gmail.com>
2018-08-05 22:41:50 +0900

    Solve 2556

---
+++ BOJ#2000~2999/src/boj2556/Main.java
@@ -0,0 +1,17 @@
+package boj2556;
+
+import java.io.*;
+
+public class Main {
+
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.valueOf(br.readLine());
+    for(int i=0; i<n; i++) {
+      for(int j=0; j<n; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
