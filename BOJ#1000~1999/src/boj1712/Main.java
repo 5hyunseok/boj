9415de1585f06748c79839bf892af948ecd105e3
5hyunseok <5xhyunseok@gmail.com>
2018-08-15 18:56:31 +0900

    Solve 1712

---
+++ BOJ#1000~1999/src/boj1712/Main.java
@@ -0,0 +1,21 @@
+package boj1712;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int a = Integer.valueOf(st.nextToken());
+    int b = Integer.valueOf(st.nextToken());
+    int c = Integer.valueOf(st.nextToken());
+    if (b >= c) {
+      System.out.println(-1);
+    } else {
+      System.out.println(a/(c-b) + 1);
+    }
+  }
+}
